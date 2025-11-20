5c9d98002a17d0e38baf6f5005666c99cc6bce59
Khasan Sabirov <johndoe@example.com>
2025-11-20 19:55:11 +0300

    Исправил вроде

--- tests/test_qw.py
+++ tests/test_qw.py
@@ -11,11 +11,12 @@ URL = "https://store.steampowered.com/"
 LOGO = By.XPATH, "//*[@placeholder='Поиск по магазину']"
 QR_LOGO = By.XPATH, "//*[@style='position: relative;']//img"
 BUTTON_LOGIN = By.XPATH, "//*[@class='global_action_link' and contains(@href, '/login/')]"
-USERNAME_INPUT = By.XPATH, "//input[@type='text' and contains(@class, 'Weup5')]"
+USERNAME_INPUT = By.XPATH, "//*[@id='responsive_page_template_content']//input[@type='text']"
 PASSWORD_INPUT = By.XPATH, "//input[@type='password']"
 BUTTON_LOGOUT = By.XPATH, "//button[@type='submit' and contains(text(), 'Войти')]"
+LOADER = By.XPATH, "//button[@type='submit' and @disabled]"
 
-ERROR_TEXT = By.XPATH, "//button[@type='submit' and contains(text(), 'Войти')]/../following-sibling::div[1]"
+ERROR_TEXT = By.XPATH, "//*[@id='responsive_page_template_content']//div[contains(text(), 'Пожалуйста, проверьте свой пароль')]"
 
 faker = Faker()
 
@@ -41,11 +42,13 @@ def test_first(driver):
 
     wait.until(EC.element_to_be_clickable(BUTTON_LOGOUT)).click()
 
+    wait.until(EC.invisibility_of_element_located(LOADER))
+
     er = wait.until(EC.visibility_of_element_located(ERROR_TEXT))
 
     actual_result = er.text
 
     expected_result = 'Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова.'
 
-    assert actual_result == expected_result
+    assert actual_result == expected_result, 'Тест успешно прошел'
 
